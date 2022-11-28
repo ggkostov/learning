3aa27e98de92f79fae09d9de069cf86bd029e191
gkostov <georgi.kostov@haemimont.com>
2022-11-28 14:03:51 +0200

    - add ArrayGenrator - array in the middle

--- src/main/java/com/haemimont/learning/arrays/ArrayGenerator.java
+++ src/main/java/com/haemimont/learning/arrays/ArrayGenerator.java
@@ -13,12 +13,11 @@ public class ArrayGenerator {
 
     public int[] generateArray() {
         List<Integer> list = new ArrayList<>(length - 1);
-        Random random = new Random();
 
         int randNum;
 
         while (list.size() < length -1) {
-            randNum = random.nextInt(length);
+            randNum = nextRandomBetween(1, length);
             if (randNum == 0) continue;
 
             if (!list.contains(randNum)) {
@@ -27,5 +26,29 @@ public class ArrayGenerator {
         }
 
         return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    public int[] arrayInTheMiddle(int start, int end) {
+        List<Integer> list = new ArrayList<>(length - 1);
+
+        int randNum;
+
+        while (list.size() < length -1) {
+            randNum = nextRandomBetween(start, end -1);
+            if (randNum == 0) continue;
+
+            if (!list.contains(randNum)) {
+                list.add(randNum);
+            }
+        }
+
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    private static int nextRandomBetween(int min, int max) {
+        Random random = new Random();
+        int number = random.nextInt(max + 1); // inclusive max
+        if (number < min) return nextRandomBetween(min, max);
+        else return number;
     }
 }
